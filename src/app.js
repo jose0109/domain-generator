19cf785a88e4f61a083f5a5870bbfbc4325568c9
jose0109 <martinezujd@gmail.com>
2022-08-24 21:39:25 +0000

    finished code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs)
+      for (let noun of nouns) {
+        for (let ext of extensions) {
+          console.log(`${pronoun}${adj}${noun}${ext}`);
+        }
+      }
+  }
 };
